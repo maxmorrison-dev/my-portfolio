b2d1e441df348010f96496869effa906ea03031d
Mauro Morittu <mauromorittu@iMac-di-Mauro.local>
2021-01-19 04:34:32 +0100

    Updates

---
+++ precache-manifest.f27a7f01c09b5b72ea0fe7fcb1987503.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0640bf776e277ea10d783a1a526a1ff",
+    "url": "/maxmorrison-dev/my-portfolio/index.html"
+  },
+  {
+    "revision": "ab034bc492ad8d1bbd38",
+    "url": "/maxmorrison-dev/my-portfolio/static/css/main.3c2f7514.chunk.css"
+  },
+  {
+    "revision": "592e888c9b28e35d7555",
+    "url": "/maxmorrison-dev/my-portfolio/static/js/2.814e151d.chunk.js"
+  },
+  {
+    "revision": "402769e0fde7682650babf6b5720df16",
+    "url": "/maxmorrison-dev/my-portfolio/static/js/2.814e151d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab034bc492ad8d1bbd38",
+    "url": "/maxmorrison-dev/my-portfolio/static/js/main.98f6d4cd.chunk.js"
+  },
+  {
+    "revision": "7a6956e196ccd2e4ccc8856708b02847",
+    "url": "/maxmorrison-dev/my-portfolio/static/js/main.98f6d4cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d376ca11ae8c5e7858df",
+    "url": "/maxmorrison-dev/my-portfolio/static/js/runtime-main.781edc19.js"
+  },
+  {
+    "revision": "f38ddea9dedadea03c5d43c596dac13f",
+    "url": "/maxmorrison-dev/my-portfolio/static/media/argon-react.f38ddea9.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/maxmorrison-dev/my-portfolio/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/maxmorrison-dev/my-portfolio/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/maxmorrison-dev/my-portfolio/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/maxmorrison-dev/my-portfolio/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/maxmorrison-dev/my-portfolio/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "1e7c70dc7d471d845aeed9e9bead0fe5",
+    "url": "/maxmorrison-dev/my-portfolio/static/media/img-1-1200x1000.1e7c70dc.jpg"
+  },
+  {
+    "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
+    "url": "/maxmorrison-dev/my-portfolio/static/media/nucleo-icons.0b8a30b1.svg"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/maxmorrison-dev/my-portfolio/static/media/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/maxmorrison-dev/my-portfolio/static/media/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/maxmorrison-dev/my-portfolio/static/media/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/maxmorrison-dev/my-portfolio/static/media/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "75f7ab18f707114d817522df8dea0a3b",
+    "url": "/maxmorrison-dev/my-portfolio/static/media/promo-1.75f7ab18.png"
+  },
+  {
+    "revision": "53033970a416368da35794389680266f",
+    "url": "/maxmorrison-dev/my-portfolio/static/media/team-1-800x800.53033970.jpg"
+  }
+]);
